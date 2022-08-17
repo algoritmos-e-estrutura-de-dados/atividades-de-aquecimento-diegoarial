bfc5e544609c3c998a1ae95974358361adcfe446
diegoarial <diego.arial@edu.unifil.br>
2022-08-17 19:09:13 -0300

    exercicios

---
+++ exercicio/average2.py
@@ -0,0 +1,7 @@
+A = input("Informe a primeira nota: ")
+B = input("Informe a segunda nota: ")
+C = input("Informe a terceira nota: ")
+
+x = ((float(A) * 2) + (float(B) * 3) + (float(C) * 5)) / 10
+
+print(f"MEDIA = {x: .1f}")
